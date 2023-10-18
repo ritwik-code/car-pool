5a637e1c693e55a267d3d241cb81d79bde9c9269
Ritwik Mishra <ritwikmishra.dev@gmail.com>
2023-10-18 21:42:22 +0300

    Adding project files

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+// import './App.css';
+import Login from './Login/Login';
+import { Route, Routes } from 'react-router-dom';
+import Carbon from './CarbonFootprint/Carbon';
+import Blog from './CarBlog/Blog';
+import UserDetails from './UserDetails/UserDetails';
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Login/>}></Route>
+        <Route path="/carbon-footprint" element={<Carbon/>}/>
+        <Route path='/car-ride-blog' element={<Blog/>}/>
+        <Route path='/user-details' element={<UserDetails/>}/>
+      </Routes>  
     </div>
   );
 }
